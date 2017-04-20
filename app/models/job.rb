7d9637c47d5cc62881d2a580a626610edc4a63ee
kkk293590509 <k293590509@gmail.com>
2017-04-20 13:42:24 +0800

    implement wage part

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,8 @@
 class Job < ApplicationRecord
   validates :title, presence: true
+
+  validates :wage_upper_bound, presence: true
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 1000}
+  validates :wage_upper_bound, numericality: { greater_than: 2000}
 end
